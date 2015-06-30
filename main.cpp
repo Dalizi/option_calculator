c01ea6c6486117cda46e1184d96ca8aa3d4ca9d7
Jiang Feng <jiangfeng1@cofco.com>
2015-06-30 17:51:52 +0800

    Changed application to read redis info from configure file.

--- main.cpp
+++ main.cpp
@@ -2,8 +2,12 @@
 #include "logindialog.h"
 #include "valuation_class.h"
 
+#include <fstream>
+
 #include <QApplication>
+#include <QCoreApplication>
 #include <QPluginLoader>
+#include <QMessageBox>
 #include <QDebug>
 
 using namespace std;
@@ -12,14 +16,28 @@ string REDIS_ADDR = "127.0.0.1";
 int REDIS_PORT = 6379;
 string REDIS_PASSWD = "";
 
+int getRedisInfo() {
+    ifstream redis_info("redis_info.ini");
+    if (!redis_info.is_open()) {
+        QMessageBox::warning(0, "Warning", "读取redis连接配置文件失败。");
+        exit(1);
+    }
+    string line;
+    if (getline(redis_info, line)) REDIS_ADDR = line;
+    if (getline(redis_info, line)) REDIS_PORT = stod(line);
+    if (getline(redis_info, line)) REDIS_PASSWD = line;
+}
+
 int main(int argc, char *argv[])
 {
-
+    getRedisInfo();
     QApplication a(argc, argv);
+    qDebug() << QCoreApplication::libraryPaths();
     DatabaseAccess db;
     LoginDialog login(&db);
     if (login.exec()){
         OptionValue calc_server("TradeDate.txt", db.getParam());
+
         MainWindow w(&db, &calc_server);
         w.show();
         return a.exec();
