ba12d554aada1401c684044bf27028d076548dee
Zehuan Tan <tanzehuan@cofco.com>
2015-07-24 20:05:10 +0800

    half done

--- positiontablemodel.cpp
+++ positiontablemodel.cpp
@@ -10,8 +10,17 @@ PositionTableModel::PositionTableModel(QObject *parent, QSqlDatabase db) :
 QVariant PositionTableModel::data(const QModelIndex & index, int role) const {
     if (role == Qt::BackgroundRole && isDirty(index)) {
         return QVariant::fromValue(QColor(Qt::red));
-    } else {
+    } else if (role == Qt::CheckStateRole && index.column() == checkableColNum)
+        return Qt::Checked;
+    else {
         return QSqlTableModel::data(index, role);
     }
 }
 
+Qt::ItemFlags PositionTableModel::flags(const QModelIndex &index) const {
+    Qt::ItemFlags flags = QSqlTableModel::flags(index);
+    if (index.column() != checkableColNum)
+        return flags;
+    else
+        return flags | Qt::ItemIsUserCheckable;
+}
--- positiontablemodel.h
+++ positiontablemodel.h
@@ -9,6 +9,9 @@ class PositionTableModel : public QSqlTableModel
 public:
     PositionTableModel(QObject *parent=0, QSqlDatabase db = QSqlDatabase());
     virtual QVariant data(const QModelIndex & index, int role = Qt::EditRole) const;
+    virtual Qt::ItemFlags flags(const QModelIndex &index) const;
+private:
+    unsigned int checkableColNum = 0;
 };
 
 #endif // POSITIONTABLEMODEL_H
